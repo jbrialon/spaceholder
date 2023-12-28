dc86878e7dd953ca4c566058a4d033c481758ee7
Jérémy Brialon <jeremzor@gmail.com>
2023-12-28 14:51:37 +0100

    use instanced mesh for particles

--- src/Experience/World/Particles.js
+++ src/Experience/World/Particles.js
@@ -33,26 +33,46 @@ export default class Particles {
     this.particle = new THREE.Object3D();
     this.geometry = new THREE.TetrahedronGeometry(2, 0);
 
-    for (var i = 0; i < 1000; i++) {
-      var mesh = new THREE.Mesh(this.geometry, this.material);
-      mesh.position
+    const particlesCount = 1000;
+
+    this.particle = new THREE.InstancedMesh(
+      this.geometry,
+      this.material,
+      particlesCount
+    );
+
+    const matrix = new THREE.Matrix4();
+    const position = new THREE.Vector3();
+    const rotation = new THREE.Euler();
+
+    for (let i = 0; i < particlesCount; i++) {
+      // Set the position and rotation for each instance
+      position
         .set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5)
-        .normalize();
-      mesh.position.multiplyScalar(90 + Math.random() * 700);
-      mesh.rotation.set(
-        Math.random() * 2,
-        Math.random() * 2,
-        Math.random() * 2
+        .normalize()
+        .multiplyScalar(90 + Math.random() * 700);
+
+      rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+
+      matrix.compose(
+        position,
+        new THREE.Quaternion().setFromEuler(rotation),
+        new THREE.Vector3(1, 1, 1)
       );
-      this.particle.add(mesh);
+
+      // Set the matrix for each instance
+      this.particle.setMatrixAt(i, matrix);
     }
+
+    this.particle.instanceMatrix.needsUpdate = true;
+
     this.scene.add(this.particle);
   }
 
   setAnimation() {}
 
   update() {
-    // Planet animation
+    // Particles animation
     this.particle.rotation.y -= 0.0004 * this.time.delta;
   }
 }
